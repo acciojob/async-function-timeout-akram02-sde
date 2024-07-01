0dde0285ff1b9c4226957f780363078e59e903c4
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-07-01 18:38:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const text = document.getElementById('text');
+const delay = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+btn.addEventListener('click', async () => {
+	output.innerText = "";
+	const message = await getData(text.value, delay.value);
+	output.innerText = message;
+})
+
+function getData(text, delay) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {resolve(text)}, delay)
+	})
+}
